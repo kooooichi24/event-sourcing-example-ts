80f0156af813b2d1b38f9c609a3bea8ea2263185
kooooichi24 <ex.kouichi1224@gmail.com>
2024-03-13 00:51:21 +0900

    feat(server.ts): add Apollo Server setup with GraphQL schema and resolvers to handle hello query

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from "@apollo/server";
+
+// The GraphQL schema
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
