08ee3a0bb243d0eef24200841acd410b741fc028
kooooichi24 <ex.kouichi1224@gmail.com>
2024-03-14 00:15:40 +0900

    feat(server.ts): add support for AWS Lambda integration using @as-integrations/aws-lambda package
    The changes in this commit add support for integrating the server with AWS Lambda using the `@as-integrations/aws-lambda` package. This allows the server to be deployed as a Lambda function and handle API Gateway proxy events.
    
    The `startServerAndCreateLambdaHandler` function from the package is used to start the Apollo Server and create a Lambda handler function. The `graphqlHandler` export is then used as the Lambda handler.
    
    Additionally, the `handlers.createAPIGatewayProxyEventV2RequestHandler()` function is passed as an argument to the `startServerAndCreateLambdaHandler` function to use the Proxy V2 handler for API Gateway events.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,8 @@
 import { ApolloServer } from "@apollo/server";
+import {
+  startServerAndCreateLambdaHandler,
+  handlers,
+} from "@as-integrations/aws-lambda";
 
 // The GraphQL schema
 const typeDefs = `#graphql
@@ -19,3 +23,10 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+// This final export is important!
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+  server,
+  // We will be using the Proxy V2 handler
+  handlers.createAPIGatewayProxyEventV2RequestHandler()
+);
