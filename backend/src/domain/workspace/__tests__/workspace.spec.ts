22de7a753d782035b15a3f87578e32bed808769d
kooooichi24 <ex.kouichi1224@gmail.com>
2024-03-24 00:44:12 +0900

    style: lint fix

--- backend/src/domain/workspace/__tests__/workspace.spec.ts
+++ backend/src/domain/workspace/__tests__/workspace.spec.ts
@@ -1,22 +1,22 @@
 import { faker } from "@faker-js/faker";
+import { Workspace } from "../workspace";
 import { WorkspaceId } from "../workspace-id";
 import { WorkspaceName } from "../workspace-name";
-import { Workspace } from "../workspace";
 
 describe("workspace", () => {
-  it("Create", () => {
-    // Given
-    const id = WorkspaceId.generate();
-    const name = WorkspaceName.of(faker.internet.userName());
+	it("Create", () => {
+		// Given
+		const id = WorkspaceId.generate();
+		const name = WorkspaceName.of(faker.internet.userName());
 
-    // When
-    const [workspace, workspaceCreated] = Workspace.create(id, name);
+		// When
+		const [workspace, workspaceCreated] = Workspace.create(id, name);
 
-    // Then
-    expect(workspace.id).toEqual(id);
-    expect(workspace.name).toEqual(name);
+		// Then
+		expect(workspace.id).toEqual(id);
+		expect(workspace.name).toEqual(name);
 
-    expect(workspaceCreated.aggregateId).toEqual(id);
-    expect(workspaceCreated.name).toEqual(name);
-  });
+		expect(workspaceCreated.aggregateId).toEqual(id);
+		expect(workspaceCreated.name).toEqual(name);
+	});
 });
--- backend/src/domain/workspace/workspace.ts
+++ backend/src/domain/workspace/workspace.ts
@@ -1,51 +1,51 @@
-import { Aggregate } from "event-store-adapter-js";
-import { WorkspaceId } from "./workspace-id";
-import { WorkspaceName } from "./workspace-name";
+import type { Aggregate } from "event-store-adapter-js";
 import { WorkspaceCreated } from "./workspace-events";
+import type { WorkspaceId } from "./workspace-id";
+import type { WorkspaceName } from "./workspace-name";
 
 export const WorkspaceTypeSymbol = Symbol("Workspace");
 
 interface WorkspaceParams {
-  id: WorkspaceId;
-  name: WorkspaceName;
-  sequenceNumber: number;
-  version: number;
+	id: WorkspaceId;
+	name: WorkspaceName;
+	sequenceNumber: number;
+	version: number;
 }
 
 export class Workspace implements Aggregate<Workspace, WorkspaceId> {
-  readonly symbol: typeof WorkspaceTypeSymbol = WorkspaceTypeSymbol;
-  readonly typeName = "Workspace";
+	readonly symbol: typeof WorkspaceTypeSymbol = WorkspaceTypeSymbol;
+	readonly typeName = "Workspace";
 
-  readonly id: WorkspaceId;
-  readonly name: WorkspaceName;
-  readonly sequenceNumber: number;
-  readonly version: number;
+	readonly id: WorkspaceId;
+	readonly name: WorkspaceName;
+	readonly sequenceNumber: number;
+	readonly version: number;
 
-  private constructor(params: WorkspaceParams) {
-    this.id = params.id;
-    this.name = params.name;
-    this.sequenceNumber = params.sequenceNumber;
-    this.version = params.version;
-  }
+	private constructor(params: WorkspaceParams) {
+		this.id = params.id;
+		this.name = params.name;
+		this.sequenceNumber = params.sequenceNumber;
+		this.version = params.version;
+	}
 
-  static create(
-    id: WorkspaceId,
-    name: WorkspaceName
-  ): [Workspace, WorkspaceCreated] {
-    const sequenceNumber = 1;
-    const version = 1;
+	static create(
+		id: WorkspaceId,
+		name: WorkspaceName,
+	): [Workspace, WorkspaceCreated] {
+		const sequenceNumber = 1;
+		const version = 1;
 
-    return [
-      new Workspace({ id, name, sequenceNumber, version }),
-      WorkspaceCreated.of(id, name, sequenceNumber),
-    ];
-  }
+		return [
+			new Workspace({ id, name, sequenceNumber, version }),
+			WorkspaceCreated.of(id, name, sequenceNumber),
+		];
+	}
 
-  withVersion(version: number): Workspace {
-    return new Workspace({ ...this, version });
-  }
+	withVersion(version: number): Workspace {
+		return new Workspace({ ...this, version });
+	}
 
-  updateVersion(versionF: (value: number) => number): Workspace {
-    return new Workspace({ ...this, version: versionF(this.version) });
-  }
+	updateVersion(versionF: (value: number) => number): Workspace {
+		return new Workspace({ ...this, version: versionF(this.version) });
+	}
 }
