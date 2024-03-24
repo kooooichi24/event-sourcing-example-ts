660ba47ee625cb05d8a28624c3d774391cd5f128
kooooichi24 <ex.kouichi1224@gmail.com>
2024-03-24 22:48:48 +0900

    test(workspace-name.spec.ts): add additional test cases for WorkspaceName class
    - Add a test case to check if an error is thrown when the value is empty.
    - Add a test case to check if an error is thrown when the value is longer than 20 characters.
    - Add test cases to check if an error is thrown when the value is not in a valid domain format.

--- backend/src/domain/workspace/__tests__/workspace-name.spec.ts
+++ backend/src/domain/workspace/__tests__/workspace-name.spec.ts
@@ -13,5 +13,45 @@ describe("workspace-name", () => {
 			// Then
 			expect(workspaceName.value).toEqual(value);
 		});
+
+		it("should throw an error when the value is empty", () => {
+			// Given
+			const value = "";
+
+			// When
+			const act = () => WorkspaceName.of(value);
+
+			// Then
+			expect(act).toThrow("WorkspaceName cannot be empty");
+		});
+
+		it("should throw an error when the value is longer than 20 characters", () => {
+			// Given
+			const value = faker.lorem.words(21);
+
+			// When
+			const act = () => WorkspaceName.of(value);
+
+			// Then
+			expect(act).toThrow("WorkspaceName cannot be longer than 20 characters");
+		});
+
+		it.each`
+			invalidValue
+			${"-domain"}
+			${"domain-"}
+		`(
+			"should throw an error when the value is not a valid domain format: $invalidValue",
+			({ invalidValue }) => {
+				// Given
+				const value = invalidValue;
+
+				// When
+				const act = () => WorkspaceName.of(value);
+
+				// Then
+				expect(act).toThrow("Invalid domain format");
+			},
+		);
 	});
 });
