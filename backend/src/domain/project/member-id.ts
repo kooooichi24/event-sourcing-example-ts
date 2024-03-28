f3247576d0e95ba1b93da2c13e8bc85712199205
kooooichi24 <ex.kouichi1224@gmail.com>
2024-03-29 00:11:28 +0900

    feat(member-id.ts): add MemberId class to represent a unique identifier for project members
    The MemberId class is added to the project domain. It represents a unique identifier for project members. It has the following features:
    - It has a private constructor that takes a string value and validates it using the uuidValidate function from the uuid library. If the value is not a valid UUID, an error is thrown.
    - It has a toJSON method that returns the value as an object.
    - It has an asString method that returns the value as a string.
    - It has a toString method that returns a string representation of the MemberId object.
    - It has an equals method that compares the value of two MemberId objects and returns true if they are equal.
    - It has a static of method that creates a new MemberId object with the given value.
    - It has a static generate method that creates a new MemberId object with a randomly generated UUID value.

---
+++ backend/src/domain/project/member-id.ts
@@ -0,0 +1,40 @@
+import * as E from "fp-ts/Either";
+import { validate as uuidValidate, v4 as uuidv4 } from "uuid";
+
+export const MemberIdTypeSymbol = Symbol("MemberId");
+
+export class MemberId {
+	readonly symbol: typeof MemberIdTypeSymbol = MemberIdTypeSymbol;
+
+	private constructor(readonly value: string) {
+		if (!uuidValidate(value)) {
+			throw new Error(`Invalid MemberId: ${value}`);
+		}
+	}
+
+	toJSON() {
+		return {
+			value: this.value,
+		};
+	}
+
+	asString() {
+		return this.value;
+	}
+
+	toString() {
+		return `${MemberId.name}(${this.value})`;
+	}
+
+	equals(anotherId: MemberId): boolean {
+		return this.value === anotherId.value;
+	}
+
+	static of(value: string): MemberId {
+		return new MemberId(value);
+	}
+
+	static generate(): MemberId {
+		return new MemberId(uuidv4());
+	}
+}
