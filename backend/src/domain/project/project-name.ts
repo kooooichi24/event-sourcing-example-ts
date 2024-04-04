f1b9287d23a2863d2b0e1bc58376e20ec6d7c9fb
kooooichi24 <ex.kouichi1224@gmail.com>
2024-04-04 23:15:34 +0900

    feat(project-name.ts): add ProjectName class to represent project names with validation and utility methods

---
+++ backend/src/domain/project/project-name.ts
@@ -0,0 +1,51 @@
+import * as E from "fp-ts/Either";
+
+export const ProjectNameTypeSymbol = Symbol("ProjectName");
+
+export class ProjectName {
+	readonly symbol: typeof ProjectNameTypeSymbol = ProjectNameTypeSymbol;
+
+	private constructor(readonly value: string) {
+		if (this.value.length === 0) {
+			throw new Error("ProjectName cannot be empty");
+		}
+
+		if (this.value.length > 80) {
+			throw new Error("SprintName cannot be longer than 80 characters");
+		}
+	}
+
+	static of(value: string): ProjectName {
+		return new ProjectName(value);
+	}
+
+
+	static validate(value: string): E.Either<string, ProjectName> {
+		try {
+			return E.right(ProjectName.of(value));
+		} catch (e) {
+			if (e instanceof Error) {
+				return E.left(e.message);
+			}
+			throw e;
+		}
+	}
+
+	equals(other: ProjectName): boolean {
+		return this.value === other.value;
+	}
+
+	toJSON() {
+		return {
+			value: this.value,
+		};
+	}
+
+	asString() {
+		return this.value;
+	}
+
+	toString() {
+		return `${ProjectName.name}(${this.value})`;
+	}
+}
