171204145cb029f457ed58f17983923f187cdd92
kooooichi24 <ex.kouichi1224@gmail.com>
2024-04-28 10:23:56 +0900

    refactor(project-events.ts): improve code formatting and consistency by aligning indentation and spacing for better readability feat(project-events.ts): add ProjectMemberAdded event type to handle adding members to a project and track the event

--- backend/src/domain/project/events/project-events.ts
+++ backend/src/domain/project/events/project-events.ts
@@ -4,94 +4,133 @@ import type { Members } from "../members";
 import type { ProjectId } from "../project-id";
 import type { ProjectName } from "../project-name";
 import type { Sprint } from "../sprint";
+import { Member } from "../member";
 
 type ProjectEventTypeSymbol =
-	| typeof ProjectCreatedTypeSymbol
-	| typeof ProjectSprintAddedTypeSymbol;
+  | typeof ProjectCreatedTypeSymbol
+  | typeof ProjectSprintAddedTypeSymbol
+  | typeof ProjectMemberAddedTypeSymbol;
 
 interface ProjectEvent extends Event<ProjectId> {
-	symbol: ProjectEventTypeSymbol;
-	toString: () => string;
+  symbol: ProjectEventTypeSymbol;
+  toString: () => string;
 }
 
 /**
  * ProjectCreated
  */
 export const ProjectCreatedTypeSymbol = Symbol("ProjectCreated");
-
 export class ProjectCreated implements ProjectEvent {
-	readonly symbol: typeof ProjectCreatedTypeSymbol = ProjectCreatedTypeSymbol;
-	readonly typeName = "ProjectCreated";
-	readonly isCreated = true;
+  readonly symbol: typeof ProjectCreatedTypeSymbol = ProjectCreatedTypeSymbol;
+  readonly typeName = "ProjectCreated";
+  readonly isCreated = true;
 
-	private constructor(
-		readonly id: string,
-		readonly aggregateId: ProjectId,
-		readonly name: ProjectName,
-		readonly members: Members,
-		readonly sequenceNumber: number,
-		readonly occurredAt: Date,
-	) {}
+  private constructor(
+    readonly id: string,
+    readonly aggregateId: ProjectId,
+    readonly name: ProjectName,
+    readonly members: Members,
+    readonly sequenceNumber: number,
+    readonly occurredAt: Date
+  ) {}
 
-	static of(
-		aggregateId: ProjectId,
-		name: ProjectName,
-		members: Members,
-		sequenceNumber: number,
-	): ProjectCreated {
-		return new ProjectCreated(
-			uuidv4(),
-			aggregateId,
-			name,
-			members,
-			sequenceNumber,
-			new Date(),
-		);
-	}
+  static of(
+    aggregateId: ProjectId,
+    name: ProjectName,
+    members: Members,
+    sequenceNumber: number
+  ): ProjectCreated {
+    return new ProjectCreated(
+      uuidv4(),
+      aggregateId,
+      name,
+      members,
+      sequenceNumber,
+      new Date()
+    );
+  }
 
-	toString() {
-		return `ProjectCreated(${this.id.toString()}, ${this.aggregateId.toString()}, ${this.name.toString()}, ${this.members.toString()}, ${
-			this.sequenceNumber
-		}, ${this.occurredAt.toISOString()})`;
-	}
+  toString() {
+    return `ProjectCreated(${this.id.toString()}, ${this.aggregateId.toString()}, ${this.name.toString()}, ${this.members.toString()}, ${
+      this.sequenceNumber
+    }, ${this.occurredAt.toISOString()})`;
+  }
 }
 
 /**
  * ProjectSprintAdded
  */
 export const ProjectSprintAddedTypeSymbol = Symbol("ProjectSprintAdded");
-
 export class ProjectSprintAdded implements ProjectEvent {
-	readonly symbol: typeof ProjectSprintAddedTypeSymbol =
-		ProjectSprintAddedTypeSymbol;
-	readonly typeName = "ProjectSprintAdded";
-	readonly isCreated = false;
+  readonly symbol: typeof ProjectSprintAddedTypeSymbol =
+    ProjectSprintAddedTypeSymbol;
+  readonly typeName = "ProjectSprintAdded";
+  readonly isCreated = false;
 
-	private constructor(
-		readonly id: string,
-		readonly aggregateId: ProjectId,
-		readonly sprint: Sprint,
-		readonly sequenceNumber: number,
-		readonly occurredAt: Date,
-	) {}
+  private constructor(
+    readonly id: string,
+    readonly aggregateId: ProjectId,
+    readonly sprint: Sprint,
+    readonly sequenceNumber: number,
+    readonly occurredAt: Date
+  ) {}
 
-	static of(
-		aggregateId: ProjectId,
-		sprint: Sprint,
-		sequenceNumber: number,
-	): ProjectSprintAdded {
-		return new ProjectSprintAdded(
-			uuidv4(),
-			aggregateId,
-			sprint,
-			sequenceNumber,
-			new Date(),
-		);
-	}
+  static of(
+    aggregateId: ProjectId,
+    sprint: Sprint,
+    sequenceNumber: number
+  ): ProjectSprintAdded {
+    return new ProjectSprintAdded(
+      uuidv4(),
+      aggregateId,
+      sprint,
+      sequenceNumber,
+      new Date()
+    );
+  }
 
-	toString() {
-		return `ProjectSprintAdded(${this.id.toString()}, ${this.aggregateId.toString()}, ${this.sprint.toString()}, ${
-			this.sequenceNumber
-		}, ${this.occurredAt.toISOString()})`;
-	}
+  toString() {
+    return `ProjectSprintAdded(${this.id.toString()}, ${this.aggregateId.toString()}, ${this.sprint.toString()}, ${
+      this.sequenceNumber
+    }, ${this.occurredAt.toISOString()})`;
+  }
+}
+
+/**
+ * ProjectMemberAdded
+ */
+export const ProjectMemberAddedTypeSymbol = Symbol("ProjectMemberAdded");
+export class ProjectMemberAdded implements ProjectEvent {
+  readonly symbol: typeof ProjectMemberAddedTypeSymbol =
+    ProjectMemberAddedTypeSymbol;
+  readonly typeName = "ProjectMemberAdded";
+  readonly isCreated = false;
+
+  private constructor(
+    readonly id: string,
+    readonly aggregateId: ProjectId,
+    readonly member: Member,
+    readonly sequenceNumber: number,
+    readonly occurredAt: Date
+  ) {}
+
+  static of(
+    aggregateId: ProjectId,
+    member: Member,
+    sequenceNumber: number
+  ): ProjectMemberAdded {
+    return new ProjectMemberAdded(
+      uuidv4(),
+      aggregateId,
+      member,
+      sequenceNumber,
+      new Date()
+    );
+  }
+
+  toString() {
+    return `ProjectMemberAdded(${this.id.toString()}, ${this.aggregateId.toString()}, ${this.member.toString()}, ${
+      this.sequenceNumber
+    }, ${this.occurredAt.toISOString()})`;
+  }
 }
