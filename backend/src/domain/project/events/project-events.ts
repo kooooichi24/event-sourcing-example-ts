25a48e841aa152dfbac6568102f8fd79e9cceeff
kooooichi24 <ex.kouichi1224@gmail.com>
2024-04-28 10:23:12 +0900

    refactor(project-events.ts): update ProjectEventTypeSymbol to include ProjectSprintAddedTypeSymbol for better event type handling feat(project-events.ts): add ProjectSprintAdded event class to represent the addition of a sprint to a project

--- backend/src/domain/project/events/project-events.ts
+++ backend/src/domain/project/events/project-events.ts
@@ -3,9 +3,11 @@ import { v4 as uuidv4 } from "uuid";
 import type { Members } from "../members";
 import type { ProjectId } from "../project-id";
 import type { ProjectName } from "../project-name";
-import type { Sprints } from "../sprints";
+import type { Sprint } from "../sprint";
 
-type ProjectEventTypeSymbol = typeof ProjectCreatedTypeSymbol;
+type ProjectEventTypeSymbol =
+	| typeof ProjectCreatedTypeSymbol
+	| typeof ProjectSprintAddedTypeSymbol;
 
 interface ProjectEvent extends Event<ProjectId> {
 	symbol: ProjectEventTypeSymbol;
@@ -27,22 +29,14 @@ export class ProjectCreated implements ProjectEvent {
 		readonly aggregateId: ProjectId,
 		readonly name: ProjectName,
 		readonly members: Members,
-		readonly sprints: Sprints,
 		readonly sequenceNumber: number,
 		readonly occurredAt: Date,
 	) {}
-
-	toString() {
-		return `ProjectCreated(${this.id.toString()}, ${this.aggregateId.toString()}, ${this.name.toString()}, ${this.members.toString()}, ${this.sprints.toString()}, ${
-			this.sequenceNumber
-		}, ${this.occurredAt.toISOString()})`;
-	}
 
 	static of(
 		aggregateId: ProjectId,
 		name: ProjectName,
 		members: Members,
-		sprints: Sprints,
 		sequenceNumber: number,
 	): ProjectCreated {
 		return new ProjectCreated(
@@ -50,9 +44,54 @@ export class ProjectCreated implements ProjectEvent {
 			aggregateId,
 			name,
 			members,
-			sprints,
 			sequenceNumber,
 			new Date(),
 		);
+	}
+
+	toString() {
+		return `ProjectCreated(${this.id.toString()}, ${this.aggregateId.toString()}, ${this.name.toString()}, ${this.members.toString()}, ${
+			this.sequenceNumber
+		}, ${this.occurredAt.toISOString()})`;
+	}
+}
+
+/**
+ * ProjectSprintAdded
+ */
+export const ProjectSprintAddedTypeSymbol = Symbol("ProjectSprintAdded");
+
+export class ProjectSprintAdded implements ProjectEvent {
+	readonly symbol: typeof ProjectSprintAddedTypeSymbol =
+		ProjectSprintAddedTypeSymbol;
+	readonly typeName = "ProjectSprintAdded";
+	readonly isCreated = false;
+
+	private constructor(
+		readonly id: string,
+		readonly aggregateId: ProjectId,
+		readonly sprint: Sprint,
+		readonly sequenceNumber: number,
+		readonly occurredAt: Date,
+	) {}
+
+	static of(
+		aggregateId: ProjectId,
+		sprint: Sprint,
+		sequenceNumber: number,
+	): ProjectSprintAdded {
+		return new ProjectSprintAdded(
+			uuidv4(),
+			aggregateId,
+			sprint,
+			sequenceNumber,
+			new Date(),
+		);
+	}
+
+	toString() {
+		return `ProjectSprintAdded(${this.id.toString()}, ${this.aggregateId.toString()}, ${this.sprint.toString()}, ${
+			this.sequenceNumber
+		}, ${this.occurredAt.toISOString()})`;
 	}
 }
