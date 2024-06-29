86304c32c7cd98c0987e361f7c3d57a34a7ae23f
kooooichi24 <ex.kouichi1224@gmail.com>
2024-06-29 17:31:53 +0900

    feat(account-repository.ts): add AccountRepository class and IAccountRepository interface to define methods for storing and finding accounts

---
+++ backend/src/domain/account/account-repository.ts
@@ -0,0 +1,23 @@
+import type * as TE from "fp-ts/TaskEither";
+import type * as O from "fp-ts/lib/Option";
+import type { Account } from "./account";
+import type { AccountId } from "./account-id";
+import type { AccountEvent } from "./account-events";
+
+export class AccountRepositoryError extends Error {
+	constructor(message: string, cause?: Error) {
+		super(message);
+		this.name = "RepositoryError";
+		this.cause = cause;
+	}
+}
+
+export interface IAccountRepository {
+	store(
+		event: AccountEvent,
+		snapshot: Account,
+	): TE.TaskEither<AccountRepositoryError, void>;
+	findById(
+		id: AccountId,
+	): TE.TaskEither<AccountRepositoryError, O.Option<Account>>;
+}
