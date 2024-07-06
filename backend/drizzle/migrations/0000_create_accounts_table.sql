d04c5d39e65370b5fe644d02a2dfd72a3ded2632
kooooichi24 <ex.kouichi1224@gmail.com>
2024-07-07 01:16:35 +0900

    feat(migrations): add initial SQL migration script to create accounts and account_roles tables

---
+++ backend/drizzle/migrations/0000_create_accounts_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS "account_roles" (
+	"role" varchar(32) PRIMARY KEY NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "accounts" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"role" varchar(32)
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "accounts" ADD CONSTRAINT "accounts_role_account_roles_role_fk" FOREIGN KEY ("role") REFERENCES "public"."account_roles"("role") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
