90141deebe34dbb05b6a328b2eb832a42e266a68
kooooichi24 <ex.kouichi1224@gmail.com>
2024-07-07 01:16:08 +0900

    feat(schema.ts): add schema definitions for accountRoles and accounts tables using drizzle-orm/pg-core to define database tables and relationships

---
+++ backend/drizzle/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, uuid, uniqueIndex, varchar } from "drizzle-orm/pg-core";
+
+export const accountRoles = pgTable("account_roles", {
+  role: varchar("role", { length: 32 }).primaryKey(),
+});
+
+export const accounts = pgTable("accounts", {
+  id: uuid("id").primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  role: varchar("role", { length: 32 }).references(() => accountRoles.role),
+});
