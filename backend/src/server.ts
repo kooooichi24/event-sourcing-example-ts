626b577361ee32e998ec1cd6911ae26a5f36dca1
kooooichi24 <ex.kouichi1224@gmail.com>
2024-07-06 17:17:18 +0900

    feat(server.ts): implement createAccount resolver to handle account creation with specified name and role

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,54 @@ import {
 	handlers,
 	startServerAndCreateLambdaHandler,
 } from "@as-integrations/aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { type EventStore, EventStoreFactory } from "event-store-adapter-js";
+import {
+	Account,
+	type AccountRole,
+	convertJSONToAccount,
+} from "./domain/account/account";
+import {
+	type AccountEvent,
+	convertJSONToAccountEvent,
+} from "./domain/account/account-events";
+import type { AccountId } from "./domain/account/account-id";
+import { AccountName } from "./domain/account/account-name";
+import { AccountRepository } from "./interface-adapter/repository/account/account-repository";
+
+const JOURNAL_TABLE_NAME = "journal";
+const SNAPSHOT_TABLE_NAME = "snapshot";
+const JOURNAL_AID_INDEX_NAME = "journal-aid-index";
+const SNAPSHOTS_AID_INDEX_NAME = "snapshots-aid-index";
+
+const dynamodbClient = new DynamoDBClient({
+	region: "ap-northeast-1",
+	endpoint: "http://127.0.0.1:18000",
+	credentials: {
+		accessKeyId: "dummy",
+		secretAccessKey: "dummy",
+	},
+});
+const eventStore: EventStore<AccountId, Account, AccountEvent> =
+	EventStoreFactory.ofDynamoDB<AccountId, Account, AccountEvent>(
+		dynamodbClient,
+		JOURNAL_TABLE_NAME,
+		SNAPSHOT_TABLE_NAME,
+		JOURNAL_AID_INDEX_NAME,
+		SNAPSHOTS_AID_INDEX_NAME,
+		32,
+		convertJSONToAccountEvent,
+		convertJSONToAccount,
+	);
+const accountRepository = AccountRepository.of(eventStore).withRetention(10);
 
 // The GraphQL schema
 const typeDefs = `#graphql
   type Query {
     hello: String
+  }
+	type Mutation {
+    createAccount(name: String, role: String): String
   }
 `;
 
@@ -15,6 +58,19 @@ const typeDefs = `#graphql
 const resolvers = {
 	Query: {
 		hello: () => "world",
+	},
+	Mutation: {
+		createAccount: async (
+			_: unknown,
+			{ name, role }: { name: string; role: AccountRole },
+		) => {
+			const [account, accountEvent] = Account.create(
+				AccountName.of(name),
+				role,
+			);
+			const result = await accountRepository.store(accountEvent, account)();
+			return result.toString();
+		},
 	},
 };
 
